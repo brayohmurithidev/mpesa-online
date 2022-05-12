4c2cbc46043c06cc2a4e0f3a83ec672bcb957931
brayohmurithidev <murithibrianm@gmail.com>
2022-05-12 14:35:14 +0300

    added mpesa online middleware

--- controllers/mpesa.js
+++ controllers/mpesa.js
@@ -30,3 +30,62 @@ exports.getOAuthToken = async (req, res, next) => {
     });
   }
 };
+
+exports.lipaNaMpesaOnline = async (req, res) => {
+  let token = req.token;
+  let auth = `Bearer ${token}`;
+
+  // Get the timestamp
+  let timestamp = require("../middleware/timestamp").timestamp;
+
+  let url = process.env.lipa_na_mpesa_url;
+  let bs_short_code = process.env.lipa_na_mpesa_shortcode;
+  let passkey = process.env.lipa_na_mpesa_passkey;
+  let password = new Buffer.from(
+    `${bs_short_code}${passkey}${timestamp}$`
+  ).toString("base64");
+
+  let transaction_type = "CustomerPayBillOnline";
+  let amount = "1";
+  let partyA = "254706134387";
+  let PartyB = process.env.lipa_na_mpesa_shortcode;
+  let PhoneNumber = "254706134387";
+  let callBackUrl = "Your_ngrok_url/mpesa/lipa na mpesa call back";
+  let accountReference = "Brian Murithi";
+  let transaction_desc = "Payment for the test program";
+
+  try {
+    let { data } = await axios
+      .post(
+        url,
+        {
+          BusinessShortCode: bs_short_code,
+          Password: password,
+          Timestamp: timestamp,
+          TransactionType: transaction_type,
+          Amount: amount,
+          PartyA: partyA,
+          PartyB: PartyB,
+          PhoneNumber: PhoneNumber,
+          CallBackUrl: callBackUrl,
+          AccountReference: accountReference,
+          TransactionDesc: transaction_desc,
+        },
+        {
+          headers: {
+            Authorization: auth,
+          },
+        }
+      )
+      .catch(console.log);
+    return res.send({
+      success: true,
+      message: data,
+    });
+  } catch (error) {
+    return res.send({
+      success: false,
+      message: error["respond"]["statusText"],
+    });
+  }
+};
